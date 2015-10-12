cb214f4e572d4bb00052160611239a505536e1e1
Frank <frank.jvr.s3@gmail.com>
2015-10-12 16:20:13 +0200

    Got the date

--- MischiefManager/app/src/main/java/com/candroid/mischiefmanager/Menu/Callendar_Fragment.java
+++ MischiefManager/app/src/main/java/com/candroid/mischiefmanager/Menu/Callendar_Fragment.java
@@ -3,25 +3,68 @@ package com.candroid.mischiefmanager.Menu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.candroid.mischiefmanager.Login.User;
+import com.candroid.mischiefmanager.Login.UserLocalStore;
 import com.candroid.mischiefmanager.R;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 /**
  * Created by Frank on 2015-09-20.
  */
 public class Callendar_Fragment extends Fragment{
     View rootview;
-    CalendarView view;
-    String user;
+    CalendarView Cview;
+    long date;
+    String dateString;
+    UserLocalStore current;
+    User loggedInUser;
+    String userDetails;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.activity_callendar, container, false);
+        Cview = (CalendarView) rootview.findViewById(R.id.calendarView);
+        current = new UserLocalStore(getContext());
+
+        loggedInUser = current.getLoggedInUser();
+        userDetails = loggedInUser.getUserDetails().get(3);
+
+    Cview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+            date = Cview.getDate();
+            if (month<10 && dayOfMonth<10)
+            {
+                dateString = String.valueOf(year)+ "-0" +String.valueOf(month) + "-0" + String.valueOf(dayOfMonth);
+            }
+            else
+            if (month<10)
+            {
+                dateString = String.valueOf(year)+ "-0" +String.valueOf(month) + "-" + String.valueOf(dayOfMonth);
+            }
+            else
+            if (dayOfMonth<10)
+            {
+                dateString = String.valueOf(year)+ "-" +String.valueOf(month) + "-0" + String.valueOf(dayOfMonth);
+            }
+            else
+            {
+
+                dateString = String.valueOf(year)+ "-" +String.valueOf(month) + "-" + String.valueOf(dayOfMonth);
+            }
+            Toast.makeText(view.getContext(),userDetails + dateString, Toast.LENGTH_SHORT).show();
+            //showToDo(userDetails, dateString);
+        }
+    });
         return rootview;
-       // ShowToDo(user, view.getDate());
     }
 }
